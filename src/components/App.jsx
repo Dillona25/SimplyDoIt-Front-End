8fe634e7487ca6c98b646a1ca59defc37444c1c6
Dillona25 <Dillyb00by49@gmail.com>
2023-11-08 21:01:21 -0600

    minor adjustments to app componenet and profilemodal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-// import Header from "../components/Header";
+import Header from "../components/Header";
 import Main from "../components/Main";
 import Nav from "../components/Nav";
 import TaskModal from "./TaskModal";
@@ -33,45 +33,10 @@ function App() {
     ]);
   };
 
-  const currentDate = new Date().toLocaleString("default", {
-    month: "long",
-    day: "numeric",
-  });
-
-  const [value, setValue] = useState("");
-
-  const handleNameChange = (e) => {
-    setValue(e.target.value);
-  };
-
   return (
     <div className="App max-w-[700px] m-auto">
-      <header className="header m-[20px]">
-        <div className="flex justify-between">
-          <h1 className="text-black  font-[Poppins] text-[35px] font-semibold self-center">
-            Simply Do It
-          </h1>
-          <div
-            className="flex flex-col content-center"
-            onClick={toggleProfileModal}
-          >
-            <img
-              className="h-[35px] w-[42px] self-center bg-center rounded-[50%]"
-              src={Avatar}
-              onClick={toggleProfileModal}
-            ></img>
-            <p className="font-[Poppins] mt-[5px]" onClick={toggleProfileModal}>
-              Hello {value}
-            </p>
-          </div>
-        </div>
-        <div className=" mt-[50px] p-[10px] border-[1px] rounded-lg">
-          <p className="self-center font-[Poppins]  text-center text-[20px] ">
-            Todays date is {currentDate}
-          </p>
-        </div>
-      </header>
-      {/* <Main /> */}
+      <Header toggleProfileModal={toggleProfileModal} />
+      <Main />
       <Nav toggleTaskModal={toggleTaskModal} />
       {activeModal === "taskModal" && (
         <TaskModal toggleCloseModal={toggleCloseModal} addTodo={addTodo} />
@@ -81,36 +46,7 @@ function App() {
       ))}
 
       {activeModal === "profileModal" && (
-        <ProfileModal toggleCloseModal={toggleCloseModal}>
-          <img
-            className=" bg-center h-[140px] w-[160px] self-center rounded-[50%] mt-[30px]"
-            src={Avatar}
-          ></img>
-          <label className="font-[Poppins] text-[20px]  mt-[30px]">
-            Change Name
-          </label>
-          <input
-            type="text"
-            placeholder="Name"
-            value={value}
-            onChange={handleNameChange}
-            className="w-[100%] border-b-[1px] border-b-black pb-[5px] font-[Poppins] mb-[10px]"
-          ></input>
-          <label className="font-[Poppins] text-[20px]  mt-[10px]">
-            Change Profile Image
-          </label>
-          <input
-            type="url"
-            placeholder="Image Url"
-            className="w-[100%] border-b-[1px] border-b-black pb-[5px] font-[Poppins] mb-[10px]"
-          ></input>
-          <button
-            type="submit"
-            className=" bg-[#8687E7] py-[5px] mt-[5px] rounded-[10px] font-[Poppins] font-semibold"
-          >
-            Save
-          </button>
-        </ProfileModal>
+        <ProfileModal toggleCloseModal={toggleCloseModal}></ProfileModal>
       )}
     </div>
   );
