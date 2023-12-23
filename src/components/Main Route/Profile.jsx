7d26e83f9d9cb51d7ebb0c5ce882871991573699
Dillona25 <Dillyb00by49@gmail.com>
2023-12-22 22:27:23 -0600

    Centered components on any device

--- src/components/Main Route/Profile.jsx
+++ src/components/Main Route/Profile.jsx
@@ -14,50 +14,54 @@ const Profile = ({
   };
 
   return (
-    <div className="p-[30px] flex flex-col mt-[50px]">
-      <h1 className="font-[Poppins] font-semibold text-[30px] text-center mt-[25px]">
-        Your Profile
-      </h1>
-      <img
-        className=" bg-center h-[180px] w-[180px] self-center rounded-[50%] mt-[30px]"
-        src={profileImage || Avatar}
-      ></img>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-[10px] w-[100%]"
-      >
-        <label className="font-[Poppins] mt-[30px]">Change Name</label>
-        <input
-          onChange={handleNameChange}
-          value={name}
-          type="text"
-          placeholder="Name"
-          className="w-[100%] bg-slate-200 p-[10px] rounded-[10px] font-[Poppins] mb-[10px]"
-        ></input>
-        <label className="font-[Poppins] mt-[10px]">Change Profile Image</label>
-        <input
-          type="link"
-          value={profileImage}
-          onChange={handleImageChange}
-          placeholder="Image Url"
-          className="w-[100%] bg-slate-200 rounded-[10px] p-[10px] font-[Poppins] mb-[10px]"
-        ></input>
-        <Link to="/" className="w-[50%] self-center">
-          <button
-            type="submit"
-            className="bg-black text-white font-[Poppins] w-[100%] py-[10px] rounded-md m-auto"
-          >
-            Save
-          </button>
+    <profile className="px-[30px] flex flex-col justify-center h-screen">
+      <div>
+        <h1 className="font-[Poppins] font-semibold text-[30px] text-center mt-[25px]">
+          Your Profile
+        </h1>
+        <img
+          className=" bg-center h-[180px] w-[180px] self-center rounded-[50%] mt-[30px] m-auto"
+          src={profileImage || Avatar}
+        ></img>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-[10px] w-[100%]"
+        >
+          <label className="font-[Poppins] mt-[30px]">Change Name</label>
+          <input
+            onChange={handleNameChange}
+            value={name}
+            type="text"
+            placeholder="Name"
+            className="w-[100%] bg-slate-200 p-[10px] rounded-[10px] font-[Poppins] mb-[10px]"
+          ></input>
+          <label className="font-[Poppins] mt-[10px]">
+            Change Profile Image
+          </label>
+          <input
+            type="link"
+            value={profileImage}
+            onChange={handleImageChange}
+            placeholder="Image Url"
+            className="w-[100%] bg-slate-200 rounded-[10px] p-[10px] font-[Poppins] mb-[10px]"
+          ></input>
+          <Link to="/" className="w-[50%] self-center">
+            <button
+              type="submit"
+              className="bg-black text-white font-[Poppins] w-[100%] py-[10px] rounded-md m-auto"
+            >
+              Save
+            </button>
+          </Link>
+        </form>
+        <Link
+          to="/"
+          className="bg-slate-200  w-[64px] h-[64px] rounded-[50%] flex justify-center m-auto mt-[50px]"
+        >
+          <button className="h-[24px] w-[24px] self-center bg-home"></button>
         </Link>
-      </form>
-      <Link
-        to="/"
-        className="bg-slate-200  w-[64px] h-[64px] rounded-[50%] flex justify-center m-auto mt-[50px]"
-      >
-        <button className="h-[24px] w-[24px] self-center bg-home"></button>
-      </Link>
-    </div>
+      </div>
+    </profile>
   );
 };
 
--- src/components/Settings route/Settings.jsx
+++ src/components/Settings route/Settings.jsx
@@ -9,77 +9,79 @@ import info from "../../images/info.svg";
 
 const Settings = ({ toggleFeedbackModal, togglePasswordModal }) => {
   return (
-    <div className="p-[30px]">
-      <h1 className="font-[Poppins] font-semibold text-[30px] text-center mt-[25px]">
-        App settings
-      </h1>
-      <h2 className="font-[Poppins] text-[15px] mt-[30px]">Account</h2>
-      <div className="flex flex-col gap-[20px] mt-[10px]">
-        <button
-          onClick={togglePasswordModal}
-          className="flex gap-[10px] bg-black p-[10px] rounded-[10px] font-[Poppins] text-white"
-        >
-          <img className="h-[24px] w-[24px]" src={key}></img>
-          Change password
-        </button>
+    <settings className="flex flex-col px-[30px] h-screen justify-center">
+      <div>
+        <h1 className="font-[Poppins] font-semibold text-[30px] text-center mt-[25px]">
+          App settings
+        </h1>
+        <h2 className="font-[Poppins] text-[15px] mt-[30px]">Account</h2>
+        <div className="flex flex-col gap-[20px] mt-[10px]">
+          <button
+            onClick={togglePasswordModal}
+            className="flex gap-[10px] bg-black p-[10px] rounded-[10px] font-[Poppins] text-white"
+          >
+            <img className="h-[24px] w-[24px]" src={key}></img>
+            Change password
+          </button>
+          <Link
+            to="/Profile"
+            className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
+          >
+            <img className="h-[24px] w-[24px]" src={user}></img>
+            Edit name & Image
+          </Link>
+          <button className="flex gap-[10px] bg-black text-red-500 p-[10px] rounded-[10px] font-[Poppins]">
+            <img className="h-[24px] w-[24px]" src={logout}></img>
+            Log out
+          </button>
+        </div>
+        <h2 className="font-[Poppins] text-[15px] mt-[30px]">The Creator</h2>
+        <div className="flex flex-col gap-[20px] mt-[10px]">
+          <Link
+            to={{ pathname: "https://arnoldsportfolio.com/" }}
+            target="_blank"
+            className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
+          >
+            <img className="h-[24px] w-[24px]" src={about}></img>
+            About the creator
+          </Link>
+          <Link
+            to={{
+              pathname: "https://www.linkedin.com/in/dillon-arnold-352782275/",
+            }}
+            target="_blank"
+            className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
+          >
+            <img className="h-[24px] w-[24px]" src={like}></img>
+            Support the creator
+          </Link>
+          <button
+            onClick={toggleFeedbackModal}
+            className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
+          >
+            <img className="h-[24px] w-[24px]" src={flash}></img>
+            Feedback and help
+          </button>
+          <Link
+            to={{
+              pathname:
+                "https://www.figma.com/file/D7FaYWal2aQlr1eQT5lJUj/UpTodo---Todo-list-app-UI-Kit-(Community)?node-id=0%3A1&mode=dev",
+            }}
+            target="_blank"
+            className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
+          >
+            <img className="h-[24px] w-[24px]" src={info}></img>
+            Original figma design
+          </Link>
+        </div>
         <Link
-          to="/Profile"
-          className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
+          to="/"
+          className="bg-slate-200  w-[64px] h-[64px] rounded-[50%] flex justify-center m-auto mt-[50px]"
         >
-          <img className="h-[24px] w-[24px]" src={user}></img>
-          Edit name & Image
+          <button className="h-[24px] w-[24px] self-center bg-home"></button>
         </Link>
-        <button className="flex gap-[10px] bg-black text-red-500 p-[10px] rounded-[10px] font-[Poppins]">
-          <img className="h-[24px] w-[24px]" src={logout}></img>
-          Log out
-        </button>
       </div>
-      <h2 className="font-[Poppins] text-[15px] mt-[30px]">The Creator</h2>
-      <div className="flex flex-col gap-[20px] mt-[10px]">
-        <Link
-          to={{ pathname: "https://arnoldsportfolio.com/" }}
-          target="_blank"
-          className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
-        >
-          <img className="h-[24px] w-[24px]" src={about}></img>
-          About the creator
-        </Link>
-        <Link
-          to={{
-            pathname: "https://www.linkedin.com/in/dillon-arnold-352782275/",
-          }}
-          target="_blank"
-          className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
-        >
-          <img className="h-[24px] w-[24px]" src={like}></img>
-          Support the creator
-        </Link>
-        <button
-          onClick={toggleFeedbackModal}
-          className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
-        >
-          <img className="h-[24px] w-[24px]" src={flash}></img>
-          Feedback and help
-        </button>
-        <Link
-          to={{
-            pathname:
-              "https://www.figma.com/file/D7FaYWal2aQlr1eQT5lJUj/UpTodo---Todo-list-app-UI-Kit-(Community)?node-id=0%3A1&mode=dev",
-          }}
-          target="_blank"
-          className="flex gap-[10px] bg-black text-white p-[10px] rounded-[10px] font-[Poppins]"
-        >
-          <img className="h-[24px] w-[24px]" src={info}></img>
-          Original figma design
-        </Link>
-      </div>
-      <Link
-        to="/"
-        className="bg-slate-200  w-[64px] h-[64px] rounded-[50%] flex justify-center m-auto mt-[50px]"
-      >
-        <button className="h-[24px] w-[24px] self-center bg-home"></button>
-      </Link>
-    </div>
+    </settings>
   );
 };
 
